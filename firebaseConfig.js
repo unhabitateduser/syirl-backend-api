35f33a1becb7adb7f9a204670ee74e52751f7b88
unhabitateduser <unhabitateduser@proton.me>
2024-07-23 21:57:40 +0200

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.app = exports.firestore = exports.auth = void 0;
+var app_1 = require("firebase-admin/app");
+var auth_1 = require("firebase-admin/auth");
+var firestore_1 = require("firebase-admin/firestore");
+var admin = require("firebase-admin");
+var serviceAccount = require("./scotland-yard-in-real-life-firebase-adminsdk-8w2mf-582ddfaee8.json");
+var app = (0, app_1.initializeApp)({
+    credential: admin.credential.cert(serviceAccount)
+});
+exports.app = app;
+var auth = (0, auth_1.getAuth)(app);
+exports.auth = auth;
+var firestore = (0, firestore_1.getFirestore)();
+exports.firestore = firestore;
