66a0ca6ab1168e25c6f76874310712e401113eca
unhabitateduser <unhabitateduser@proton.me>
2024-11-23 16:39:58 +0100

    Add files via upload

--- app.ts
+++ app.ts
@@ -1,29 +1,31 @@
 
 const express= require("express")
-require('dotenv').config()
 const cors = require("cors");
+import { addDoc, collection, doc, onSnapshot, setDoc } from "firebase/firestore";
 import { auth, firestore } from "./firebaseConfig";
 
+require('dotenv').config()
 
-let lastLocation1:(LocationObjectCoords & {userID: string})[] = []
-let lastLocation2:(LocationObjectCoords & {userID: string})[] = []
-let loc = 1
+
+
 
 // Normaly in prod should be passed by parent, each server thread handles a game 
 const GameID =process.env.GC // shareCode
 let GameDoc: null | GameType = null
 let GameUsers: null | GameUsersType = null
 let Imposters: string[] = []
-
 const USERS: {
     [key: string]: string;
 } = {}
 const LOCATIONS: GameUsersType = {}
 const updateInterval = 5000  // in ms
-
-
 const startMillis = Date.now()
 let NextShowTime: number = startMillis + 0 * 60 * 1000
+let HidePhase = false
+let UpdateCallbackTime = Date.now()
+let updateTimeCallback: NodeJS.Timeout  | null = null;
+
+
 
 // types
 type LatLng = {
@@ -54,7 +56,7 @@ export type GameUsersType = {
 export type GameUserType = {
     banned: boolean;
     dbID: string;
-    location: ClientUpdate;
+    locations: ClientUpdate[];
     IDToken: string;
     imposter: string
 }
@@ -73,10 +75,12 @@ type LocationObjectCoords = {
     speed: number;
 }
 
+
+
 // create listener to game doc
-let documentRef = firestore.doc('games/'+GameID);
-const unsubscribeGameDoc = documentRef.onSnapshot(documentSnapshot => {
-    if (documentSnapshot.exists) {
+let documentRef = doc(firestore, 'games/'+GameID);
+const unsubscribeGameDoc = onSnapshot(documentRef, documentSnapshot => {
+    if (documentSnapshot.exists()) {
         // @ts-ignore
         const data: GameType = documentSnapshot.data()
         //console.log(data)
@@ -85,8 +89,8 @@ const unsubscribeGameDoc = documentRef.onSnapshot(documentSnapshot => {
         // game was deleted, discard this process, handled automatically ??
     }
   });
-  let collectionRef = firestore.collection('games/' + GameID + "/users");
-  const unsubscribeGameUsersCol = collectionRef.onSnapshot(collectionSnapshot => {
+  let collectionRef = collection(firestore, 'games/' + GameID + "/users");
+  const unsubscribeGameUsersCol = onSnapshot(collectionRef, collectionSnapshot => {
     // parse data
     // use collectionSnapshot.docChanges to modify permissions ??
     // parse and convert to key value dict
@@ -108,22 +112,30 @@ const unsubscribeGameDoc = documentRef.onSnapshot(documentSnapshot => {
 
 
 
+
+
+
+// server setup
 const app = express();
 const PORT = 4058;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//👇🏻 New imports
+// use websocket on top
 const http = require("http").Server(app);
+import { Server } from "socket.io";
 
-const SocketIO = require("socket.io")
-
-const GameServer = new SocketIO.Server(http, {
+const GameServer = new Server(http, {
     cors: {
         origin: "*" //<http://localhost:3000>
     }
 });
+
+
+
+
+
 
 
 GameServer.use((socket, next) => {
@@ -138,9 +150,14 @@ GameServer.on('connection', async (socket) => {
         // get secret user token from headers
         const IDToken = socket.handshake.auth.token
         // verify user
-        const decodedIdToken = await auth.verifyIdToken(IDToken)
-        // get user info
-        const user = await auth.getUser(decodedIdToken.uid)
+        const decodeResponse = await fetch("https://syirl-auth-backend.netlify.app/.netlify/functions/verify", {
+            method: "POST",
+            body: JSON.stringify({
+                token: IDToken
+            })
+        })
+        const userID = await decodeResponse.text()
+       
 
         
         // verify user and authorisations
@@ -152,16 +169,16 @@ GameServer.on('connection', async (socket) => {
 
 
         // check if user is allowed to join game
-        if (user.uid in GameUsers) {
+        if (userID in GameUsers) {
             // check that if user is banned
-            if (GameUsers[user.uid].banned) {
+            if (GameUsers[userID].banned) {
                 // this user is banned, destroy socket
                 throw Error("banned user")
             }
 
             // for private games
             if (!GameDoc.public) {
-                if (GameDoc.authorizedUsers.includes(user.uid)) {
+                if (GameDoc.authorizedUsers.includes(userID)) {
                     // authorized user
                 }
                  else {
@@ -172,26 +189,41 @@ GameServer.on('connection', async (socket) => {
             // authorized user
             // add user to list
             // @ts-ignore
-            USERS[socket.id] = user.uid
+            USERS[socket.id] = userID
             // @ts-ignore
-            LOCATIONS[user.uid] = {IDToken, banned: false}
+            LOCATIONS[userID] = {IDToken, banned: false, locations: []}
+
         }  else if (GameDoc.public) {  // check if game is public
             // add player to list of players
-            firestore.doc(`games/${GameID}/users/${user.uid}`).create({
+            setDoc(doc(firestore, `games/${GameID}/users/${userID}`), {
                 banned: false,
 				imposter: false
             })
             // authorized user
             // add user to list
             // @ts-ignore
-            USERS[socket.id] = user.uid
+            USERS[socket.id] = userID
             // @ts-ignore
-            LOCATIONS[user.uid] = {IDToken, banned: false}
+            LOCATIONS[userID] = {IDToken, banned: false, locations: []}
         } else {
             //console.log(GameUsers)
             // user doenst exist, error
             throw Error("invalid user")
         }
+
+        // successfull!
+        // need to send all past players positions
+
+
+        const locations: {[id:string]: LocationObjectCoords[]} = {}
+        for (const userID of Object.keys(LOCATIONS)) {
+            if (!LOCATIONS[userID].locations || !GameDoc || LOCATIONS[userID].locations.length == 0) continue
+   
+            locations[userID] = LOCATIONS[userID].locations.map(loc => loc.coords)
+    
+         
+        }
+        socket.emit('all-time-player-locations', locations)
         
     }
     catch(error) {
@@ -204,6 +236,7 @@ GameServer.on('connection', async (socket) => {
 
     console.log(`⚡: ${socket.id} verified user just connected!`);
 
+
     socket.on('disconnect', () => {
       socket.disconnect()
       console.log('🔥: A user disconnected');
@@ -213,11 +246,12 @@ GameServer.on('connection', async (socket) => {
     socket.on("location", (data: ClientUpdate) => {
         // @ts-ignore
         if (!(socket.id in USERS)) return console.log("unverified user")
+            console.log("new player location")
         // @ts-ignore
         const userID = USERS[socket.id]
         //@ts-ignore
-        LOCATIONS[userID].location = data.location
-        LOCATIONS[userID].location.timestamp = Date.now()
+        LOCATIONS[userID].locations.push(data.location)
+        LOCATIONS[userID].locations[LOCATIONS[userID].locations.length-1].timestamp = Date.now()
 
         
         // @ts-ignore
@@ -241,7 +275,7 @@ GameServer.on('connection', async (socket) => {
 
     socket.on("si", () => {
         if (Imposters.includes(USERS[socket.id]))
-            HH = false
+            HidePhase = false
     })
 
     socket.on("setNextShowTime", (data) => {
@@ -260,57 +294,76 @@ setTimeout(() => {
     // https://stackoverflow.com/questions/4647348/send-message-to-specific-client-with-socket-io-and-node-js
 }, 10000)
 
-let HH = false
-let NEXT = Date.now()
+
+
+
+// infinite function callback to start and end when the imposter is visible
+
 function updateTime() {
     if (!GameDoc) {
-        setTimeout(updateTime, 10000)
+        updateTimeCallback = setTimeout(updateTime, 10000)
         return
     }
     
-    if (HH) {
-		HH = false
-        NEXT = Date.now() + GameDoc.imposterShowTime
-        setTimeout(updateTime, GameDoc.imposterShowTime
-      
-);
+    if (HidePhase) {
+		HidePhase = false
+        UpdateCallbackTime = Date.now() + GameDoc.imposterShowTime
+        updateTimeCallback = setTimeout(updateTime, GameDoc.imposterShowTime);
+
+        // send to all players
+        console.log("sending imposter visibility update")
+        GameServer.emit("imposter-visiblity", {
+            nextHidePhase: Math.ceil(UpdateCallbackTime/1000), // currently in show
+            nextShowPhase: Math.ceil((UpdateCallbackTime + GameDoc.imposterHideTime)/1000)
+        })
+        
     } else {
-        HH = true
-        NEXT = Date.now() + GameDoc.imposterHideTime
-		setTimeout(updateTime, GameDoc.imposterHideTime)
+        HidePhase = true
+        UpdateCallbackTime = Date.now() + GameDoc.imposterHideTime
+		updateTimeCallback = setTimeout(updateTime, GameDoc.imposterHideTime)
+
+        
+        // send to all players
+        console.log("sending imposter visibility update 2")
+        GameServer.emit("imposter-visiblity", {
+            nextShowPhase: Math.ceil(UpdateCallbackTime/1000), // currently in hide
+            nextHidePhase: Math.ceil((UpdateCallbackTime + GameDoc.imposterShowTime)/1000)
+        })
     }
     
 }
 updateTime()
 
-function verifyTimeForImp (millis: number) {
-    if (!GameDoc) return false
-    console.log(4)
-    if (millis > NextShowTime) {
-        if (millis < NextShowTime + GameDoc.imposterShowTime) {
-            // in show time: send coordinates
-            GameServer.emit("showI", (NextShowTime + GameDoc.imposterShowTime) -millis
-            )
-            console.log(1)
-            return true
-        } else {
-            // ran out of show time: don't show coordinates and update next showTime
-            NextShowTime = Date.now() + GameDoc.imposterHideTime
-            GameServer.emit("hideI",NextShowTime-millis)
-            console.log("2")
-        }
-    } else {
-        // in hide time: don't send coordinates
-        GameServer.emit('hideI', (NextShowTime + GameDoc.imposterHideTime)-millis)
-        console.log(3)
-    }
-    return false
-    
-}
 
-// send every 2 secs location update
+
+// function verifyTimeForImp (millis: number) {
+//     if (!GameDoc) return false
+//     console.log(4)
+//     if (millis > NextShowTime) {
+//         if (millis < NextShowTime + GameDoc.imposterShowTime) {
+//             // in show time: send coordinates
+//             GameServer.emit("showI", (NextShowTime + GameDoc.imposterShowTime) -millis
+//             )
+//             console.log(1)
+//             return true
+//         } else {
+//             // ran out of show time: don't show coordinates and update next showTime
+//             NextShowTime = Date.now() + GameDoc.imposterHideTime
+//             GameServer.emit("hideI",NextShowTime-millis)
+//             console.log("2")
+//         }
+//     } else {
+//         // in hide time: don't send coordinates
+//         GameServer.emit('hideI', (NextShowTime + GameDoc.imposterHideTime)-millis)
+//         console.log(3)
+//     }
+//     return false
+    
+// }
+
+// SEND LOCATION OF ALL PLAYERS
+// send every x secs location update
 function sendPlayersLocation() {
-    console.log("sending udpate")
     const locations: {[id:string]: LocationObjectCoords} = {}
     
     const millis = Date.now()
@@ -318,44 +371,43 @@ function sendPlayersLocation() {
     
 
     for (const userID of Object.keys(LOCATIONS)) {
-        if (!LOCATIONS[userID].location || !GameDoc) continue
+        if (!LOCATIONS[userID].locations || !GameDoc || LOCATIONS[userID].locations.length == 0) continue
         //console.log(Imposters, userID )
+        const lastLocationPos = LOCATIONS[userID].locations.length -1
         if (Imposters.includes(userID)) {
-           if(!HH) 
-            locations[userID] = LOCATIONS[userID].location.coords
+           if(!HidePhase) 
+            locations[userID] = LOCATIONS[userID].locations[lastLocationPos].coords
         }
-        else if (LOCATIONS[userID].location.timestamp >millis -5000)
-            locations[userID] = LOCATIONS[userID].location.coords
+        else if (LOCATIONS[userID].locations[lastLocationPos].timestamp >millis -5000)
+            locations[userID] = LOCATIONS[userID].locations[lastLocationPos].coords
 
      
     }
-            if (!HH) {
-                
-                GameServer.emit("hideI", NEXT - millis)
-            }else {
-                GameServer.emit("showI", NEXT-millis)
-            }
         
         
     
     //console.log("sending now",locations, millis)
-    GameServer.emit("players-location", locations)
-    lastLocation1 = []
+    console.log("sending udpate", locations)
+    if (Object.keys(locations).length > 0 ) GameServer.emit("players-location", locations)
+    
     setTimeout(sendPlayersLocation, updateInterval)
 }
 setTimeout(sendPlayersLocation, updateInterval)
 
 
+
+
+
 app.use(cors());
 
-app.get("/api", (req, res) => {
+app.get("/api", (req: any, res: any) => {
     console.log("/api")
     res.json({
         message: "Hello world",
     });
 });
 
-app.post("/api/bck-loc-update", (req, res) => {
+app.post("/api/bck-loc-update", (req: any, res: any) => {
     const {userID, authToken, locationData} = req.body
     console.log("/api/bck-loc-update", req.body)
     if (typeof userID != "string" || typeof authToken != "string" || typeof locationData != "object") {
@@ -374,7 +426,7 @@ app.post("/api/bck-loc-update", (req, res) => {
     // verified user
 
 
-    LOCATIONS[USERS[userID]].location = locationData
+    LOCATIONS[USERS[userID]].locations.push(locationData)
 
     res.statusCode = 200
     res.end()
--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,18 +1,43 @@
-import { initializeApp } from 'firebase-admin/app';
-import { getAuth } from 'firebase-admin/auth';
-import { getFirestore } from 'firebase-admin/firestore';
-var admin = require("firebase-admin");
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import firebase from "firebase/compat/app";
+import { getAuth } from "firebase/auth";
+
+// Required for side-effects
+import "firebase/firestore";
 
 
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://support.google.com/firebase/answer/7015592
+const firebaseConfig = {
+
+    apiKey: "AIzaSyCT16vnwbfzaAOGLqGnAb_lw24-gh_A94w",
+  
+    authDomain: "scotland-yard-in-real-life.firebaseapp.com",
+  
+    projectId: "scotland-yard-in-real-life",
+  
+    storageBucket: "scotland-yard-in-real-life.appspot.com",
+  
+    messagingSenderId: "87421038815",
+  
+    appId: "1:87421038815:web:6633e1badad171000ef96a",
+  
+    measurementId: "G-V121WZJTQN"
+  
+  };
+  
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 
-var serviceAccount = require("./fsa.json");
+// Initialize Cloud Firestore and get a reference to the service
+const firestore = getFirestore(app);
 
-const app = initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-});
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
 
-const auth = getAuth(app)
-const firestore = getFirestore()
+
 
 export { auth, firestore, app }
